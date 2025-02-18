0acaf21161aaa3fd7f3d0536db8fb5296aaf17c7
YuOhshima <bigislandyu31@gmail.com>
2025-02-19 07:14:15 +0900

    string commit

---
+++ src/Chapter1/lesson4.py
@@ -0,0 +1,21 @@
+s = "test"
+print(s)
+
+print("hello.\n How are you?")
+print(r"C:\name\name")
+
+print(
+    """\
+line1
+line2
+line3\
+"""
+)
+
+print("Hi." * 3 + "Mike")
+
+print("Py" "thon")
+prifix = "Py"
+print(prifix + "thon")
+conect = "aaaaaaaaa" "bbbbbbbbbbb"
+print(conect)
