6ea9573c3541859f436971d24f71e2e9d8728540
YuOhshima <bigislandyu31@gmail.com>
2025-02-23 19:58:17 +0900

    If文判定

---
+++ src/Chapter3/lesson1.py
@@ -0,0 +1,12 @@
+#  コメントは基本的に下につけるよ
+print("XXXX")
+"""
+test
+test
+test
+"""
+# test
+print("XXXX")
+
+# Apple price
+some_value = 100
---
+++ src/Chapter3/lesson2.py
@@ -0,0 +1,8 @@
+s = "aaaaaaa" + "bbbbbbb"
+print(s)
+
+# １行 80文字以上の場合は次の行に行くべき！
+x = 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
+print(x)
+
+w = "aaaaaaaaa" + "bbbbbbbbbb"
